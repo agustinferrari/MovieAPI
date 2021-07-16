463069e6543ba5e079a5217b48801b9dee9b6ab1
Agustin Ferrari <agustin_ferrari@hotmail.com>
2021-07-16 19:03:07 -0300

    implements login and adds false login test

--- DataAccess/userDataAccess.js
+++ DataAccess/userDataAccess.js
@@ -1,6 +1,24 @@
+const fs = require('fs');
+
 class UserDataAccess {
   constructor(pathToData) {
     this.pathToData = pathToData;
+  }
+
+  readData() {
+    this.userData = fs.readFileSync(this.pathToData);
+  }
+
+  login(emailToCheck, passwordToCheck) {
+    this.readData();
+    const users = JSON.parse(this.userData);
+    for (let i = 0; i < users.length; i++) {
+      const userIterator = users[i];
+      if (userIterator.email === emailToCheck && userIterator.password === passwordToCheck) {
+        return true;
+      }
+    }
+    return false;
   }
 }
 
--- Testing/userDataAccess.test.js
+++ Testing/userDataAccess.test.js
@@ -3,27 +3,48 @@ const {UserDataAccess} = require('./../DataAccess/UserDataAccess.js');
 const pathToUsersFile = 'usersTest.txt';
 let dataAccess;
 
-const testData = '{' +
+const testData = '[{' +
     '"email": "juanasanchez@gmail.com",' +
     '"firstName": "Juana",' +
     '"lastName": "Sanchez",' +
     '"password": "password12345"' +
- '}';
+ '},'+
+ '{' +
+    '"email": "pepep@gmail.com",' +
+    '"firstName": "Pepe",' +
+    '"lastName": "Gonzales",' +
+    '"password": "424pass2343421"' +
+ '}]';
 
 beforeEach(() => {
   usersFileInitialize();
 });
 
+afterEach(() => {
+  usersFileEmtpy();
+});
 
 function usersFileInitialize() {
   dataAccess = new UserDataAccess(pathToUsersFile);
-  fs.writeFile(pathToUsersFile, testData, function(err, result) {
+  fs.writeFileSync(pathToUsersFile, testData, function(err, result) {
     if (err) {
-      console.log('Error trying to write on ' +pathToUsersFile, err);
+      console.log('Error trying to write on ' + pathToUsersFile, err);
+    }
+  });
+}
+
+function usersFileEmtpy() {
+  fs.writeFileSync(pathToUsersFile, '', function(err, result) {
+    if (err) {
+      console.log('Error trying to write on ' + pathToUsersFile, err);
     }
   });
 }
 
 test('User already registered login', () =>{
   expect(dataAccess.login('juanasanchez@gmail.com', 'password12345')).toBeTruthy();
+});
+
+test('User not registered login', () =>{
+  expect(dataAccess.login('jaime@gmail.com', 'superSecurePass')).toBeFalsy();
 });
