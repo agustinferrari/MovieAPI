d467c508446c2f7f41775421e6e815331f257687
Agustin Ferrari <agustin_ferrari@hotmail.com>
2021-07-18 15:15:16 -0300

    Implements token generator

---
+++ Logic/UserController.js
@@ -0,0 +1,17 @@
+class UserController {
+  constructor() {
+  }
+
+  generateToken() {
+    const charArray = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'.split('');
+    const tokenArray = [];
+    const tokenLength = 20;
+    for (let i=0; i<tokenLength; i++) {
+      const j = (Math.random() * (charArray.length-1)).toFixed(0);
+      tokenArray[i] = charArray[j];
+    }
+    return tokenArray.join('');
+  }
+}
+
+module.exports = {UserController: UserController};
--- UnitTest/userController.test.js
+++ UnitTest/userController.test.js
@@ -7,6 +7,7 @@ beforeEach(() => {
 
 test('Generate token is alphanumeric and 20 characters long', () =>{
   const token = userController.generateToken();
-  const regex = /^[a-z0-9]{20}$/i;
-  expect(userDataAccess.login('juanasanchez@gmail.com', 'password12345')).toBeTruthy();
+  const regexAlphanumeric20Chars = /^[a-z0-9]{20}$/i;
+  expect(regexAlphanumeric20Chars.test(token)).toBeTruthy();
 });
+
